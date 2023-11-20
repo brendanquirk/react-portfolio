b1034880809e0fed5ff185699a6dc158a6669733
brendanquirk <qui1217@gmail.com>
2023-11-20 10:24:31 -0500

    navbar complete

---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import Logo from '../assets/logo.png'
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  const handleClick = () => {
+     setNav(!nav)
+  }
+
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0A192F] text-gray-300'>
+      <div>
+        <img src={Logo} alt='logo' style={{width:'200px'}}/>
+      </div>
+      {/* Menu */}
+      <ul className='hidden md:flex'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+      {/* Hamburger */}
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <FaBars/> : <FaTimes/>}
+      </div>
+      {/* Mobile Menu */}
+      <div className={!nav ? 'hidden': 'absolute top-0 left-0 w-full h-screen bg-[#0A192F] flex flex-col justify-center items-center'}>
+        <ul>
+          <li className='py-6 text-4xl'>Home</li>
+          <li className='py-6 text-4xl'>About</li>
+          <li className='py-6 text-4xl'>Skills</li>
+          <li className='py-6 text-4xl'>Work</li>
+          <li className='py-6 text-4xl'>Contact</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
